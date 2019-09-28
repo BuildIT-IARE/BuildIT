4f6c46381fa302fc57f9ffe9a5a209b1681410f1
Flozender <tayeeb.hasan@gmail.com>
2019-09-28 16:59:35 +0530

    validateSubmit

--- Server/routes/submission.route.js
+++ Server/routes/submission.route.js
@@ -3,9 +3,6 @@ let middleware = require('../util/middleware.js');
 module.exports = (app) => {
     const submissions = require('../controllers/submission.controller.js');
 
-    // Create a new submission
-    app.post('/submissions', submissions.create);
-
     // Retrieve submissions with contestId
     app.get('/submissions/:questionId', middleware.checkToken, submissions.findAll);
 
