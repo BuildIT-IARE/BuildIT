af15c99041386e92dee6e52f3d3843501dc70dc6
focusblast <abu.jaleel30@gmail.com>
2019-10-03 11:56:04 +0530

    changes to admin panel

--- Client/index.js
+++ Client/index.js
@@ -58,6 +58,9 @@ app.get('/questionadmin', async (req, res) => {
 app.get('/contestadmin', async (req, res) => {
   res.render('contestAdminPanel');
 });
+app.get('/admin', async (req, res) => {
+  res.render('admin');
+});
 
 app.get('/ide/:questionId', async (req, res) => {
   let questionId = req.params.questionId;
