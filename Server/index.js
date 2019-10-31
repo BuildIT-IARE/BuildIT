d3e8d01258881f3ad7ef5847508405d5cfe1e783
flozender <tayeeb.hasan@gmail.com>
2019-10-31 15:27:38 +0530

    date

--- Server/index.js
+++ Server/index.js
@@ -130,9 +130,11 @@ app.post('/isOngoing', middleware.checkToken, async(req, res) => {
     if (req.decoded.admin){
       accepted = true;
     }
+    console.log(moment(duration.date.toString()).isAfter(today, 'day'));
     if (moment(duration.date.toString()).isAfter(today, 'day')){
       accepted = true;
     }
+
     res.send({
       success: accepted,
       message: "Contest window isn't open!"
