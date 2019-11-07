6a12fd2381e8ab700a366df485d703f294926e3c
focusblast <abu.jaleel30@gmail.com>
2019-11-08 04:32:38 +0530

    local run

--- IDE/js/ide.js
+++ IDE/js/ide.js
@@ -1,4 +1,4 @@
-var apiUrl = localStorageGetItem("api-url") || "https://api.judge0.com";
+var apiUrl = localStorageGetItem("api-url") || "http://119.235.53.59:3000";
 var serverUrl = "http://119.235.53.59:5000";
 var wait = localStorageGetItem("wait") || false;
 var pbUrl = "https://pb.judge0.com";
