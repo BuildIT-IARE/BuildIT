038919566c5ecac32b4ffcc89a47b7a7096bfcf5
Your Name <boyapatikrishna99@gmail.com>
2020-01-03 19:06:07 +0530

    questionTut.route.js

---
+++ Server/routes/questionTut.route.js
@@ -0,0 +1,23 @@
+let middleware = require('../util/middleware.js');
+
+module.exports = (app) => {
+    const questions = require('../controllers/questionTut.controller.js');
+
+    // Create a new question
+    app.post('/questions', middleware.checkTokenAdmin, questions.create);
+
+    // Retrieve all questions
+    app.get('/questions', middleware.checkTokenAdmin, questions.findAll);
+
+    // Retrieve a single question with questionId
+    app.get('/questions/:questionId', middleware.checkToken, questions.findOne);
+
+    // Retrieve all questions with contestId
+    app.get('/questions/contests/:contestId', middleware.checkToken, questions.findAllContest);
+
+    // Update a question with questionId
+    app.put('/questions/:questionId', middleware.checkTokenAdmin, questions.update);
+
+    // Delete a question with questionId
+    app.delete('/questions/:questionId', middleware.checkTokenAdmin, questions.delete);
+}
