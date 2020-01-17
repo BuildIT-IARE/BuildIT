291ac1d8f01e1879e8c19fbcf519a18583f8388a
flozender <tayeeb.hasan@gmail.com>
2020-01-17 20:59:26 +0530

    admin

--- Client/index.js
+++ Client/index.js
@@ -110,6 +110,31 @@ app.get('/admin/add/question', async (req, res) => {
   });
 });
 
+app.get('/admin/add/pdf', async (req, res) => {
+  let url = {
+    url: clientRoute,
+    serverurl: serverRoute
+  };
+
+  let options = {
+    url : serverRoute + '/isAdmin',
+    method: 'get',
+    headers: {
+      'authorization': req.cookies.token
+    },
+    json: true
+  };
+
+  request(options, function(err, response, body){
+    if (body.success){
+      res.render('uploadpdf', {data: url});
+    } else {
+      body.message = "Unauthorized access";
+        res.render('error', {data: body, imgUsername: req.cookies.username})
+    }    
+  });
+});
+
 app.get('/admin/add/contest', async (req, res) => {
 
   let options = {
