bfc4a9d352e599feda2ae0efb1023cbb7f692548
Krishna Chandra Boyapati <41461972+JethroGibbsN@users.noreply.github.com>
2020-02-11 04:00:14 +0530

    autopush

--- Client/index.js
+++ Client/index.js
@@ -749,11 +749,11 @@ app.get('/verify', async (req, res) => {
 
 // Tutorials Code
 
-app.get('/tutorials', async(req, res) => {
-  body = {};
-  body.message = "Coming Soon!";
-  res.render('error', {data: body, imgUsername: req.cookies.username});
-});
+// app.get('/tutorials', async(req, res) => {
+//   body = {};
+//   body.message = "Coming Soon!";
+//   res.render('error', {data: body, imgUsername: req.cookies.username});
+// });
 
 app.get('/flipClass', async (req, res) => {
   let url = {
@@ -763,7 +763,7 @@ app.get('/flipClass', async (req, res) => {
   res.render('flipClass', {imgUsername: req.cookies.username, data: url});
 });
 
-app.get('/tutorials2', async (req, res) => {
+app.get('/tutorials', async (req, res) => {
   let options = {
     url : serverRoute + '/courses',
     method: 'get',
