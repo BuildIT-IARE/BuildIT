897d29aa6a7efd8846c66b9bb15deac8d1f0ed9c
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-02-14 11:18:44 +0530

    autopush

--- Server/controllers/question.controller.js
+++ Server/controllers/question.controller.js
@@ -12,7 +12,7 @@ exports.create = (req, res) => {
         });
     }
 
-    Question.find({contestId: "*"})
+    Question.find({contestId:""})
     .then(questions => {
         let currQuestions = questions.length + 1;
         req.body.questionId = "IARE" + currQuestions.toString();
