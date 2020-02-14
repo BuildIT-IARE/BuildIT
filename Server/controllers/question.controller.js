d040933ffe01cd8baf99fb1171434d40f2cb4577
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-02-14 11:18:02 +0530

    autopush

--- Server/controllers/question.controller.js
+++ Server/controllers/question.controller.js
@@ -12,7 +12,7 @@ exports.create = (req, res) => {
         });
     }
 
-    Question.find()
+    Question.find({contestId: "*"})
     .then(questions => {
         let currQuestions = questions.length + 1;
         req.body.questionId = "IARE" + currQuestions.toString();
@@ -175,7 +175,66 @@ exports.createTutorials = (req, res) => {
         });
     });
 };
+exports.createTutorials = (req, res) => {
+    // Validate request
+    if(!req.body.questionName) {
+        return res.status(400).send({
+            success: false,
+            message: "Question name can not be empty"
+        });
+    }
 
+    Question.find()
+    .then(questions => {
+        let currQuestions = questions.length + 1;
+        req.body.questionId = "IARE" + currQuestions.toString();
+
+            // Create a Question
+            const question = new Question({
+                questionId: req.body.questionId,
+                questionName: req.body.questionName,
+                contestId: req.body.contestId,
+                questionDescriptionText: req.body.questionDescriptionText, 
+                questionInputText: req.body.questionInputText,
+                questionOutputText: req.body.questionOutputText,
+                questionExampleInput1: req.body.questionExampleInput1,
+                questionExampleOutput1: req.body.questionExampleOutput1,
+                questionExampleInput2: req.body.questionExampleInput2,
+                questionExampleOutput2: req.body.questionExampleOutput2,
+                questionExampleInput3: req.body.questionExampleInput3,
+                questionExampleOutput3: req.body.questionExampleOutput3,
+                questionHiddenInput1: req.body.questionHiddenInput1,
+                questionHiddenInput2: req.body.questionHiddenInput2,
+                questionHiddenInput3: req.body.questionHiddenInput3,
+                questionHiddenOutput1: req.body.questionHiddenOutput1,
+                questionHiddenOutput2: req.body.questionHiddenOutput2,
+                questionHiddenOutput3: req.body.questionHiddenOutput3,
+                questionExplanation: req.body.questionExplanation,
+                author: req.body.author,
+                editorial: req.body.editorial,
+                difficulty: req.body.level,
+                language: req.body.language,
+                conceptLevel: req.body.sublevel,
+                courseId: ["IARE_PY", "IARE_C", "IARE_CPP", "IARE_JAVA"]
+                });
+    
+        // Save Question in the database
+        question.save()
+        .then(data => {
+            res.send(data);
+        }).catch(err => {
+            res.status(500).send({
+                success: false,
+                message: err.message || "Some error occurred while creating the Question."
+            });
+        });
+    }).catch(err => {
+        res.status(500).send({
+            success: false,
+            message: err.message || "Some error occurred while retrieving questions."
+        });
+    });
+};
 
 exports.createTutorialsExcel = (req, res) => {
     if(req.files.upfile){
