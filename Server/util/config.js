4d8bb289db254635607af0da66640329653f4622
Krishna Chandra Boyapati <41461972+JethroGibbsN@users.noreply.github.com>
2020-03-22 21:13:20 +0530

    fp

--- Server/util/config.js
+++ Server/util/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     localServer: true,
     secret: 'IIdhfdSHIO42425UUdncxdf952S',
-    clientAddress: 'http://119.235.53.59:80',
-    serverAddress: 'http://119.235.53.59:5000',
+    clientAddress: 'http://13.234.234.30:80',
+    serverAddress: 'http://13.234.234.30:5000',
     apiAddress: 'https://api.judge0.com/',
-    localAPI: 'http://119.235.53.59:3000',
+    localAPI: 'http://13.234.234.30:3000',
     dbURL: 'mongodb://localhost/BuildIT',
     OAuthClientID: '1064096911787-mkih3p6r7f6p2tcc24i267736mu8qljj.apps.googleusercontent.com',
     OAuthClientSecret: 'Qs2c7YP8Q8jCMw-PIbARUrmn',
