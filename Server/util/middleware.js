4606f8df214c3065aa13ebe7e1e68e82811b6344
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-03-24 09:23:11 +0530

    autopush

--- Server/util/middleware.js
+++ Server/util/middleware.js
@@ -42,7 +42,8 @@ let checkTokenAdmin = (req, res, next) => {
   let token = req.cookies.token || req.headers['x-access-token'] || req.headers['authorization']; 
   // Express headers are auto converted to lowercase
   // req.headers['x-access-token'] || req.headers['authorization'];
-  console.log(req.cookies,req.body,req.headers);
+  console.log(token);
+
   if (token) {
     if (token.startsWith('Bearer ')) {
       // Remove Bearer from string
