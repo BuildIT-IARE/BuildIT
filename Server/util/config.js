cb7daec3713891a8f11a8b3795337d358911e5f3
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-07-20 13:08:39 +0530

    OAuth2

--- Server/util/config.js
+++ Server/util/config.js
@@ -8,7 +8,7 @@ module.exports = {
   dbURL: "mongodb://localhost/BuildIT",
   OAuthClientID:
     "1064096911787-mkih3p6r7f6p2tcc24i267736mu8qljj.apps.googleusercontent.com",
-  OAuthClientSecret: "Qs2c7YP8Q8jCMw-PIbARUrmn",
+  OAuthClientSecret: "7Qp4p9dWyt3aRZYTaf6AWF6o",
   OAuthRefreshToken:
-    "1//04XEQXx_iEG72CgYIARAAGAQSNwF-L9Ir9XRvFe7XM6QPG8DM5Rgnd14TQHMf2fbgTGiLZk5ak6QfSFcp98jgdjACTtWR4oMAH_A",
+    "1//04jLWuvCVvvkHCgYIARAAGAQSNwF-L9IraGUzfN2dn0hBrPFlt1-dUwdfJbKT3MNc_9ypsWk0pnBaD92v8XqzWJ-3gjgtV7WYWLs",
 };
