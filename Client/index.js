8e4d805f8a13d72df5cebf8a963496d4a17a2d8c
Avinash Vytlagit <avinashvytla351@gmail.com>
2022-02-02 10:43:37 +0530

    tutorials practice

--- Client/index.js
+++ Client/index.js
@@ -10,10 +10,10 @@ const xlsx = require("xlsx");
 const fs = require("fs");
 const fetch = require("node-fetch");
 var _ = require("lodash");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 // Load config
-dotenv.config({ path: '../Server/util/config.env' });
+dotenv.config({ path: "../Server/util/config.env" });
 
 let serverRoute = process.env.serverAddress;
 let clientRoute = process.env.clientAddress;
@@ -80,30 +80,30 @@ app.post("/skill", async (req, res) => {
   ];
 
   let options = {
-	  url: serverRoute + "/weeks",
-	  method: "get",
-	  headers: {
-		authorization: req.cookies.token,
-	  },
-	  json: true,
+    url: serverRoute + "/weeks",
+    method: "get",
+    headers: {
+      authorization: req.cookies.token,
+    },
+    json: true,
   };
-  
-  request(options, async(err, response, week) => {
+
+  request(options, async (err, response, week) => {
     let options = {
       url: serverRoute + "/skill",
       method: "get",
       headers: {
-      authorization: req.cookies.token,
+        authorization: req.cookies.token,
       },
       json: true,
     };
 
-    if( req.body.weekId !== undefined ) {
+    if (req.body.weekId !== undefined) {
       options.url = serverRoute + "/skill/" + req.body.weekId;
     }
-    
-    request(options, async(err, response, body) => {
-      if(!err) {
+
+    request(options, async (err, response, body) => {
+      if (!err) {
         const ordered = _.orderBy(
           body,
           function (item) {
@@ -118,18 +118,19 @@ app.post("/skill", async (req, res) => {
           ordered[2]["rollNumber"],
         ];
 
-        const [firstResponse, secondResponse, thirdResponse] = await Promise.all([
-          fetch(`${serverRoute}/users/branch/${toppers[0]}`),
-          fetch(`${serverRoute}/users/branch/${toppers[1]}`),
-          fetch(`${serverRoute}/users/branch/${toppers[2]}`),
-        ]);
+        const [firstResponse, secondResponse, thirdResponse] =
+          await Promise.all([
+            fetch(`${serverRoute}/users/branch/${toppers[0]}`),
+            fetch(`${serverRoute}/users/branch/${toppers[1]}`),
+            fetch(`${serverRoute}/users/branch/${toppers[2]}`),
+          ]);
 
         const first = await firstResponse.json();
         const second = await secondResponse.json();
         const third = await thirdResponse.json();
-    
+
         const topperData = [first, second, third];
-        
+
         body.clientAddress = clientRoute;
 
         res.render("leaderboard", {
@@ -145,8 +146,8 @@ app.post("/skill", async (req, res) => {
           imgUsername: req.cookies.username,
         });
       }
-    })
-  })
+    });
+  });
 });
 
 app.get("/admin/add/skillup", async (req, res) => {
@@ -254,7 +255,7 @@ app.post("/editProfile", async (req, res) => {
     },
     json: true,
   };
-  
+
   request(options, function (err, response, body) {
     if (body.success) {
       res.redirect("/profile");
@@ -264,7 +265,7 @@ app.post("/editProfile", async (req, res) => {
         imgUsername: req.cookies.username,
       });
     }
-  });  
+  });
 });
 
 app.get("/login", async (req, res) => {
@@ -879,7 +880,7 @@ app.get("/admin/unverifiedusers", async (req, res) => {
     json: true,
   };
   request(options, function (err, response, body) {
-    var unverified_users=[];
+    var unverified_users = [];
     for (let i = 0; i < body.length; i++) {
       if (!body[i].isVerified && !body[i].admin) {
         body[i].color = "pink";
@@ -1717,7 +1718,7 @@ app.get("/qualifierTestScore/:contestId", async (req, res) => {
         json: true,
       };
 
-      request(options, (err, response, body) => { 
+      request(options, (err, response, body) => {
         if (!body.message) {
           const color = new Map([
             [0, "black"],
@@ -1731,7 +1732,10 @@ app.get("/qualifierTestScore/:contestId", async (req, res) => {
             colors[j] = color.get(body.coding[j].score);
           }
 
-          body.codingScore = body.coding.reduce((sum, curr) => sum + curr.score, 0);
+          body.codingScore = body.coding.reduce(
+            (sum, curr) => sum + curr.score,
+            0
+          );
 
           body.color = colors;
           body.answers = [
@@ -2189,10 +2193,20 @@ app.get("/tutorials/:courseId/:difficulty", async (req, res) => {
       }
       // console.log(body, "\n ____________________________________________________________________");
       // console.log(bodytimer);
-      res.render("displayTutQuestions", {
-        imgUsername: req.cookies.username,
-        data: body,
-      });
+      if (
+        req.params.difficulty != "topics" &&
+        req.params.difficulty != "compaines"
+      ) {
+        res.render("displayTutQuestions", {
+          imgUsername: req.cookies.username,
+          data: body,
+        });
+      } else {
+        res.render("practiceTutList", {
+          imgUsername: req.cookies.username,
+          title: req.params.difficulty,
+        });
+      }
     });
   });
 });
@@ -2283,7 +2297,6 @@ app.get("/certificate", async (req, res) => {
     };
 
     request(options, (err, response, body2) => {
-
       if (!body2.message) {
         res.render("certificate", {
           imgUsername: req.cookies.username,
