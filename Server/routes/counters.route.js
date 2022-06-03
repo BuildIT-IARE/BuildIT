e4c79372273da43fab4ea35435a31b2c14959daa
Avinash Vytlagit <avinashvytla351@gmail.com>
2022-06-03 13:46:49 +0530

    count

--- Server/routes/counters.route.js
+++ Server/routes/counters.route.js
@@ -1,7 +1,5 @@
 module.exports = (app) => {
   const counters = require("../controllers/counters.controller.js");
 
-  app.post("/counters/add", counters.addDayCount);
 
-  app.get("/counters", counters.getAllCounts);
 };
