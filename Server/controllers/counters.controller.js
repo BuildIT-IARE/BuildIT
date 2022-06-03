46d14e533ec88b6ce9574f243f7cca3a243678aa
Jayanth Naidu <71186065+noobguy77@users.noreply.github.com>
2022-06-03 13:55:09 +0530

    Revert "Count"

--- Server/controllers/counters.controller.js
+++ Server/controllers/counters.controller.js
@@ -1,5 +1,4 @@
 const Counter = require("../models/counters.model.js");
-const Count = require("../models/count.model.js")
 var today = new Date();
 
 exports.addDayCount = (req, res) => {
@@ -37,12 +36,3 @@ exports.getAllCounts = (req, res) => {
     });
 };
 
-exports.getCounts = (req, res)=>{
-  Count.find({})
-  .then((count)=>{
-    res.send(count)
-  })
-  .catch((err)=>{
-    res.send("Error")
-  })
-}
