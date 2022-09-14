7fe09e3dba137a269e70b1cf5259cbaa47a783a1
noobguy77 <impmailonly7@gmail.com>
2022-09-14 10:55:48 +0530

    skillUp Controller updated(due to GFG UI Changes)

--- IDE/js/ide.js
+++ IDE/js/ide.js
@@ -1,5 +1,5 @@
 var apiUrl = localStorageGetItem("api-url") || "http://13.234.234.30:3000";
-var serverUrl = "http://13.234.234.30:5000";
+var serverUrl = "http://localhost:5000";
 var wait = localStorageGetItem("wait") || false;
 var pbUrl = "https://pb.judge0.com";
 var check_timeout = 200;
