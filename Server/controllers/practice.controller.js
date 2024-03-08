2a8e67e52081d03240e6f7d677fd4dab10dd203a
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-03-08 21:43:50 +0530

    almost finish practice feat

--- Server/controllers/practice.controller.js
+++ Server/controllers/practice.controller.js
@@ -1,35 +1,76 @@
 const Question = require("../models/question.model.js");
-const Submission = require("./submission.controller.js")
+const Submission = require("../models/submission.model.js")
 
-const regex = /^PRACTICE____/;
 
 const servePracticePage = async (req, res) => {
     try {
+        const regExpPattern = new RegExp(req.body.username, "i");
         // Find questions matching the specified pattern
         const questions = await Question.find({ questionId: { "$regex": "PRACTICE" } });
 
-        // Iterate over each question
-        // for (const question of questions) {
-        //     // Check if the user has submitted an answer for this question
-        //     const userSubmission = await Submission.findUser(req.cookies.username, question.questionId);
+        const userSubmissions = await Submission.find({
+            "questionId": { "$regex": "PRACTICE" },
+            "username": { "$regex": regExpPattern },
+        });
 
-        //     // Set the status of the question based on user submission
-        //     question.status = userSubmission ? true : false;
-        // }
 
-        // Send the modified questions as the response
-        res.send(questions);
+        console.log(userSubmissions)
+
+        let userSubmissionsMap = {}
+        
+        userSubmissions.forEach((submission) => {
+            if (!userSubmissionsMap.hasOwnProperty(submission.questionId)) {
+                userSubmissionsMap[submission.questionId] = submission.score;
+            } else {
+                userSubmissionsMap[submission.questionId] = Math.max(userSubmissionsMap[submission.questionId], submission.score);
+            }
+        });
+
+
+
+        const payload = []
+
+        questions.forEach((question) => {
+            let statusCheck = '';
+            if (userSubmissionsMap.hasOwnProperty(question.questionId)) {
+                const score = userSubmissionsMap[question.questionId];
+                if (score == 100) {
+                    statusCheck = 'Solved'
+                }
+                else if (score > 0) {
+                    statusCheck = 'Partially Solved'
+                }
+                else {
+                    statusCheck = 'Unsolved'
+                }
+            }
+            else {
+                statusCheck = 'Unsolved'
+            }
+
+            const payloadInstance = {
+                questionId: question.questionId,
+                questionName: question.questionName,
+                difficulty: question.difficulty,
+                estimateTime: question.estimateTime,
+                status: statusCheck,
+                tags: question.tags,
+            }
+
+            payload.push(payloadInstance)
+        })
+        res.send(payload);
     } catch (err) {
         // Handle errors
         res.status(500).send('Something went wrong: ' + err);
     }
 }
 
+
+
 const sendQuestion = async (req, res) => {
-    console.log('aaa')
     try {
         const data = await Question.findOne({ questionId: req.params.id });
-        console.log('aaa' + data)
         res.send(data)
     }
     catch (err) {
